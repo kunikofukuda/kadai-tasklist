c2bbc152fa9a417b147661a82d3fcbb387173a94
EC2 Default User <ec2-user@ip-172-31-20-91.us-east-2.compute.internal>
2018-06-07 05:47:13 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::resource('tasks', 'tasksController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
